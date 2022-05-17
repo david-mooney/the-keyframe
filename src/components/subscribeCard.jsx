345f834a38bb9906c03ad2a934fcc748b28a499e
David Mooney <dmooney@hubspot.com>
2022-05-17 15:16:31 +0100

    testing netlify form detection

--- src/components/subscribeCard.jsx
+++ src/components/subscribeCard.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import * as styles from './subscribeCard.module.css';
 
 const SubscribeCard = () => {
+  const formName = 'subscribe';
   // const emailForm = document.querySelector('.email-form');
   // if (emailForm) {
   //   emailForm.addEventListener('submit', e => {
@@ -12,8 +13,9 @@ const SubscribeCard = () => {
 
   return (
     <div className={styles.card}>
-      <form className={styles.form} name="subscribe" method="POST" data-netlify="true">
+      <form className={styles.form} name={formName} method="POST" data-netlify="true">
         <h3>Subscribe for the latest posts</h3>
+        <input type="hidden" name={formName} value="Subscription Form" />
         <input type="email" name="email" placeholder="Your email" required />
         <button type="submit">Subscribe</button>
         <small>I will never share your email and you can unsubscribe at any time</small>
