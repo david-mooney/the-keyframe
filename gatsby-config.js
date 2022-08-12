b59bd5d3134cc89f9c04487c8db37f3ca5eeef2a
david-mooney <2527460+david-mooney@users.noreply.github.com>
2022-08-12 19:08:20 +0100

    preact removed

--- gatsby-config.js
+++ gatsby-config.js
@@ -126,7 +126,7 @@ module.exports = {
         },
       },
     },
-    'gatsby-plugin-preact',
+    // 'gatsby-plugin-preact', // preact disabled until compatible with React v18
     'gatsby-plugin-offline',
     {
       resolve: `gatsby-plugin-algolia`,
