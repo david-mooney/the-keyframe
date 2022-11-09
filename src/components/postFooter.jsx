946ec2b76ee05c532e31044cebadc91b3048f23b
david-mooney <>
2022-11-09 21:40:04 +0000

    a lotta stuff

--- src/components/postFooter.jsx
+++ src/components/postFooter.jsx
@@ -2,17 +2,13 @@ import React from 'react';
 import PostCard from './cards/postCard.jsx';
 import * as styles from './postFooter.module.css';
 
-const PostFooter = ({ data }) => {
-  const { previous, next } = data;
-
-  return (
-    <nav className={styles.container} title="Blog posts navigation">
-      <ul className={styles.list}>
-        <li>{previous && <PostCard {...previous} />}</li>
-        <li>{next && <PostCard {...next} />}</li>
-      </ul>
-    </nav>
-  );
-};
+const PostFooter = ({ data }) => (
+  <nav className={styles.container} title="Blog posts navigation">
+    <ul className={styles.list}>
+      <li>{data.previous && <PostCard {...data.previous} />}</li>
+      <li>{data.next && <PostCard {...data.next} />}</li>
+    </ul>
+  </nav>
+);
 
 export default PostFooter;
--- src/templates/blog-post.jsx
+++ src/templates/blog-post.jsx
@@ -54,7 +54,7 @@ export const pageQuery = graphql`
       html
       frontmatter {
         title
-        date(formatString: "MMMM DD, YYYY")
+        date(formatString: "DD MMMM YYYY")
         description
         image {
           childImageSharp {
