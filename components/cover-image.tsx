7b218d5892c8070a2f31d1a8cd0fca485a261b29
david-mooney <2527460+david-mooney@users.noreply.github.com>
2023-03-08 23:50:33 +0000

    testing new design

--- components/cover-image.tsx
+++ components/cover-image.tsx
@@ -1,22 +1,14 @@
-import Image from 'next/image';
 import styles from './cover-image.module.css';
 
 type Props = {
-  title: string;
-  src: string;
+  title?: string;
+  src?: string;
   priority?: boolean;
 };
 
-const CoverImage = ({ title, src, priority }: Props) => (
-  <div className={styles.square}>
-    <Image
-      priority={priority}
-      className={styles.image}
-      src={src}
-      alt={`Cover Image for ${title}`}
-      width={250}
-      height={250}
-    />
+const CoverImage = ({}: Props) => (
+  <div className={styles.frame}>
+    <div className={styles.image}></div>
   </div>
 );
 
