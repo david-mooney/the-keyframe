0814ace6649c9c292792d74347a4e7d06c127c4c
david-mooney <>
2023-03-14 22:35:10 +0000

    tweaked fuzzy search logic

--- lib/fuzzy-search.ts
+++ lib/fuzzy-search.ts
@@ -3,23 +3,26 @@ import Fuse from 'fuse.js';
 import { posts } from '../cache/posts-cache';
 
 const getAllPostsWithTags = (query: string) => {
-  const allTags = getAllTags();
+  const tags = getAllTags();
+  const queryTags = query.split(' ').filter((tag) => tags.includes(tag));
 
-  const tagsInQuery = allTags.filter((tag) => {
-    return query.includes(tag);
-  });
+  if (queryTags.length === 0) {
+    return posts;
+  }
 
-  return posts.filter((post) => {
-    if (tagsInQuery.length === 0) return true;
-    return tagsInQuery.some((tag) => post.tags.includes(tag));
-  });
+  const postsWithQueryTags = posts.filter((post) =>
+    post.tags.some((tag) => queryTags.includes(tag))
+  );
+
+  return postsWithQueryTags;
 };
 
 const fuzzySearch = (query: string) => {
   const postsWithQueryTags = getAllPostsWithTags(query);
 
   const fuse = new Fuse(postsWithQueryTags, {
-    includeScore: true,
+    findAllMatches: true,
+    ignoreLocation: true,
     keys: ['title', 'excerpt', 'tags'],
   });
 
