c90e9db7f4beae92c410710b5faf0962c99b1aed
david-mooney <2527460+david-mooney@users.noreply.github.com>
2023-09-24 16:55:25 +0100

    fixed style lint runner

--- components/layout/footer.tsx
+++ components/layout/footer.tsx
@@ -3,19 +3,17 @@ import styles from '@components/layout/footer.module.css';
 
 const { publicRuntimeConfig } = getConfig();
 
-const Footer = () => {
-  return (
-    <footer className={styles.footer}>
-      <div className={styles.row}>
-        <small className={styles.copyright}>
-          All rights reserved © David Mooney {new Date().getFullYear()}
-        </small>
-        <small aria-hidden="true">
-          Version: {publicRuntimeConfig?.version}
-        </small>
-      </div>
-    </footer>
-  );
-};
+const currentYear = new Date().getFullYear();
+
+const Footer = () => (
+  <footer className={styles.footer}>
+    <div className={styles.row}>
+      <small className={styles.copyright}>
+        All rights reserved © David Mooney {currentYear}
+      </small>
+      <small aria-hidden="true">Version: {publicRuntimeConfig?.version}</small>
+    </div>
+  </footer>
+);
 
 export default Footer;
